f3959cabf8fe99b6fa02fb72c72efd4b89b3eba6
Atsuno <achiptsuno@gmil.com>
2023-10-16 10:48:23 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Frontend Mentor | News homepage',
+  description: 'Challenge News homepage',
+}
+
+const PageHome = () => (
+  <main className="flex min-h-screen flex-col items-center justify-center gap-10">
+    a
+  </main>
+)
+
+export default PageHome
---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import daisyui from 'daisyui'
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['var(--font-primary)'],
+        secondary: ['var(--font-secondary)'],
+      },
+      colors: {
+        specific: '#F0F0F0',
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [{
+      light: {},
+    }],
+    darkTheme: 'light',
+    logs: false,
+  },
+}
+
+export default config
